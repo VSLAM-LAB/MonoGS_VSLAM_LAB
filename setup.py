ab45a00d8648a044adb1d75b5b024919f0dc2c1b
alejandrofontan <fontanvillacampa@gmail.com>
2025-12-23 14:10:57 +1000

    update: update setup.py configuration

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="monogs",
+    version="1.0.0",
+    description="MonoGS Visual SLAM system",
+    packages=find_packages(),
+    py_modules=["vslamlab_monogs_mono"], 
+    install_requires=[
+        "numpy",
+        "torch",
+    ],
+    include_package_data=True,
+    package_data={
+        'gui.gl_render': ['shaders/*.glsl'],
+    },
+    entry_points={
+        "console_scripts": [
+            "vslamlab_monogs_mono = vslamlab_monogs_mono:main",
+        ],
+    },
+)
